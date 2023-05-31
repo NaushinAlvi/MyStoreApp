c85ff92f73ba3e7a212e2ff584267e4529949cfc
NaushinAlvi <alvi800007@gmail.com>
2023-05-31 23:52:48 +0530

    first commit

--- admin_panel/pages/index.js
+++ admin_panel/pages/index.js
@@ -1,5 +1,5 @@
 export default function Home() {
     return (
-        <div className="bg-blue-933 A-screen h-screen"></div>
+        <div className="bg-blue-900 w-screen h-screen"></div>
     )
 }
